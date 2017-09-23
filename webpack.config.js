3998d997e71e944be87bdda33d1097901e2f1e99
Tay Moore <taycmoore2009@gmail.com>
2017-09-22 19:42:18 -0500

    Update packages to include less

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
  context: path.join(__dirname, 'src'),
  entry: [
    './main.js',
+   './main.less'
  ],
  output: {
    path: path.join(__dirname, 'www'),
@@ -11,13 +12,23 @@ module.exports = {
  },
  module: {
    rules: [
-     {
-       test: /\.js$/,
-       exclude: /node_modules/,
-       use: [
-         'babel-loader',
-       ],
-     },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+        'babel-loader',
+      ],
+    },
+    {
+      test: /\.less$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+          loader: "css-loader" // translates CSS into CommonJS
+      }, {
+          loader: "less-loader" // compiles Less to CSS
+      }]
+    }
    ],
  },
  resolve: {
